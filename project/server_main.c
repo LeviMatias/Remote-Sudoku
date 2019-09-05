76f70d26ae2e4e2ee681effc35ccee547ced3aa9
unknown <matilevi35@gmail.com>
2019-09-05 19:31:20 -0300

    completed preliminar server-client loop

--- project/server_main.c
+++ project/server_main.c
@@ -18,9 +18,11 @@ int server_network_component_listen_for_client(networkcomp_t* self){
 				r = protocol_receive(&(self->protocol), &(self->protocol.msg[1]), (r - 1));
 			}
 			if ( id_res > 0 && r > 0){// check again because it may have requested more bytes
-				sudoku_msg_t sudoku_rv = interpreter_execute_command(&interpreter, &sudoku, self->protocol.msg);
-				//send length
-				//sendmsg
+				sudoku_message_t* sudoku_rv = interpreter_execute_command(&interpreter, &sudoku, self->protocol.msg);
+				uint32_t size = sudoku_rv->size;
+				size = htonl(size);
+				protocol_send(&(self->protocol), (char*)&size, sizeof(uint32_t));
+				protocol_send(&(self->protocol), sudoku_rv->text, sudoku_rv->size);
 			}
 		}
 	}
